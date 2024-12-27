9c8f1479dc97915d1b6bb9805a4f35e22ff057ee
jahan-code <jahanzaib9599@gmail.com>
2024-12-27 18:14:15 +0500

    error

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const res = await axios.post('https://wave-liart.vercel.app/api/v1/user/register', input, {
+      const res = await axios.post('http://localhost:8000/api/v1/user/register', input, {
         headers: {
           'Content-Type': 'application/json'
         },
