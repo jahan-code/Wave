5df75a0cc17a33b4db04477d4c2e515fe4b57376
jahan-code <jahanzaib9599@gmail.com>
2025-03-25 04:45:14 +0500

    responsiveness

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,16 +4,22 @@ import Feed from './Feed'
 import { Outlet } from 'react-router-dom'
 import useGetAllPost from '@/hooks/useGetAllPost'
 import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers'
+
 const Home = () => {
   useGetAllPost();
   useGetSuggestedUsers();
+  
   return (
     <div className='flex'>
-        <div className='flex-grow'>
+      {/* Main content area */}
+      <div className='flex-grow w-full min-w-0'>  {/* Added min-w-0 for better responsive behavior */}
         <Feed/>
         <Outlet/>
-        </div>
-        <RightSidebar/>
+      </div>
+      
+      <div className="hidden md:block md:min-w-[300px] lg:min-w-[350px]">  {/* Added minimum width */}
+        <RightSidebar />
+      </div>
     </div>
   )
 }
