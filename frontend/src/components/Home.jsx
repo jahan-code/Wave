1f75b50b2f5a5a05ce63db65cd121d4166b238e0
jahan-code <jahanzaib9599@gmail.com>
2025-03-25 04:56:04 +0500

    responsiveness

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -15,12 +15,12 @@ const Home = () => {
       {/* Main content area */}
       <div className='flex-grow w-full min-w-0'>  {/* Added min-w-0 for better responsive behavior */}
         <Feed/>
+      <SuggestedUser/>
         <Outlet/>
       </div>
       
       <div className="hidden md:block md:min-w-[300px] lg:min-w-[350px]">  {/* Added minimum width */}
         <RightSidebar />
-      <SuggestedUser/>
       </div>
     </div>
   )
