584d9207960e8e190a41ce94b8ac5f6d83bdfa32
jahan-code <jahanzaib9599@gmail.com>
2025-03-26 03:04:58 +0500

    responsiveness

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,7 +3,7 @@ import Posts from './Posts'
 
 const Feed = () => {
   return (
-    <div className='md:mx-0 md:my-0 mx-18 my-18'>
+    <div className='md:mx-0 md:my-0 mx-18 my-18 pt-[60px] pb-[60px]'>
         <Posts className='flex-1 my-8 flex flex-col items-center pl-[20%]'/>
     </div>
   )
