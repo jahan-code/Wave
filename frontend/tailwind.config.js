b8ce646bb07c84ee12e5ab3102d3b2a3175b29da
jahan-code <jahanzaib9599@gmail.com>
2025-03-25 05:24:38 +0500

    responsiveness

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,12 +8,19 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'ig-blue': '#0095f6',
+        'ig-gray': '#737373',
+        'ig-stories-start': '#fdf497',
+        'ig-stories-middle': '#fdf497',
+        'ig-stories-end': '#fd5949',
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      colors: {},
+      
     },
   },
   plugins: [tailwindcssAnimate],
