2870087808c57321b5da5f5ef1cdec0895f45820
jahan-code <jahanzaib9599@gmail.com>
2025-04-23 13:29:58 +0500

    redeploy

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -159,7 +159,7 @@ const LeftSidebar = () => {
                                   <AvatarFallback>CN</AvatarFallback>
                                 </Avatar>
                                 <p className="text-sm">
-                                  <span className="font-bold">{notification.userDetails?.username}</span> like your post
+                                  <span className="font-bold">{notification.userDetails?.username}</span> liked your post
                                 </p>
                               </div>
                             ))
